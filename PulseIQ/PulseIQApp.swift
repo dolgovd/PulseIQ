651e829452f5df646de728a3181b59850a26c8cb
Dmitry Dolgov <d.dolgov@outlook.com>
2026-04-24 18:11:03 +0200

    Initial Commit

---
+++ PulseIQ/PulseIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseIQApp.swift
+//  PulseIQ
+//
+//  Created by dima on 24-04-2026.
+//
+
+import SwiftUI
+
+@main
+struct PulseIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
